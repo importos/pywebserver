8267560e1c67b97c0986d7b6a4b8e621619f3153
importos <m.j_banitaba@yahoo.com>
2016-07-15 19:07:11 +0430

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,3 +8,21 @@
 # Copyright:   (c) User 2016
 # Licence:     GNU
 #-------------------------------------------------------------------------------
+
+def main():
+    import HTTPHandler
+    import SocketServer
+    PORT = 80
+    server_address=('',PORT)
+    handler = HTTPHandler.HTTPHandler
+    server = SocketServer.TCPServer(server_address, handler)
+    print "serving at port", PORT
+    server.serve_forever()
+    pass
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        import traceback as tr
+        tr.print_exc()
