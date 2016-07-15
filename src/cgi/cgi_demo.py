ddef9d73c4118e6dfb02b58df8e2b926cb661ee9
importos <m.j_banitaba@yahoo.com>
2016-07-15 19:06:26 +0430

    Update cgi_demo.py

--- src/cgi/cgi_demo.py
+++ src/cgi/cgi_demo.py
@@ -8,3 +8,26 @@
 # Copyright:   (c) User 2016
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+import cgi
+import cgitb; cgitb.enable()
+import os
+__SERVER__=os.environ
+__PATH__=__SERVER__['PATH_INFO']
+__DOC_ROOT__=__SERVER__['DOCUMENT_ROOT']
+
+
+def load_doc():
+    pp=os.path.abspath(__DOC_ROOT__+__PATH__+"index.html")
+    if os.path.exists(pp):
+        hf=open(pp,'rb')
+        data=hf.read()
+        hf.close()
+        return data
+    return pp + str()
+message=load_doc()
+
+print "Content-type: text/html"
+print "Content-Length:", str(len(message))
+print
+
+print message
